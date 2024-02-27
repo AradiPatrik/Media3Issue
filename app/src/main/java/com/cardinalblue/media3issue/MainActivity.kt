944e33ddb33a41385a32f229aec239b9e7fb8a11
Patrik Aradi <aradipatrik@gmail.com>
2024-02-27 10:39:44 +0800

    task1

--- app/src/main/java/com/cardinalblue/media3issue/MainActivity.kt
+++ app/src/main/java/com/cardinalblue/media3issue/MainActivity.kt
@@ -7,14 +7,26 @@ import android.os.Bundle
 import android.view.View
 import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.lifecycle.lifecycleScope
 import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaItem.ClippingConfiguration
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.transformer.Composition
+import androidx.media3.transformer.EditedMediaItem
+import androidx.media3.transformer.EditedMediaItemSequence
+import androidx.media3.transformer.ExportException
+import androidx.media3.transformer.ExportResult
+import androidx.media3.transformer.Transformer
 import com.cardinalblue.media3issue.databinding.ActivityMainBinding
 import com.cardinalblue.media3issue.dsl.getFileFromAssets
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
 import java.io.File
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 class MainActivity : AppCompatActivity() {
 
@@ -60,7 +72,52 @@ class MainActivity : AppCompatActivity() {
     }
 
     private suspend fun renderMovie(): Uri {
-        TODO("Implement this")
+        val bankruptcy = getFileFromAssets("bankruptcy.mp4")
+        val loydosan = getFileFromAssets("loydosan.mp4")
+
+        val bankruptcyMediaItem = MediaItem.Builder()
+            .setUri(Uri.fromFile(bankruptcy))
+            .build()
+
+        val loydosanMediaItem = MediaItem.Builder()
+            .setUri(Uri.fromFile(loydosan))
+            .build()
+
+        val editedBankruptcy = EditedMediaItem.Builder(bankruptcyMediaItem)
+            .build()
+
+        val editedLoydosan = EditedMediaItem.Builder(loydosanMediaItem)
+            .build()
+
+        val sequence = EditedMediaItemSequence(editedBankruptcy, editedLoydosan)
+        val composition = Composition.Builder(sequence)
+            .build()
+
+        @Suppress("BlockingMethodInNonBlockingContext")
+        val tempFile = File.createTempFile("output", ".mp4")
+
+        suspendCancellableCoroutine<Unit> {
+            val transformer = Transformer.Builder(this)
+                .build()
+            transformer.addListener(object : Transformer.Listener {
+                override fun onCompleted(composition: Composition, exportResult: ExportResult) {
+                    super.onCompleted(composition, exportResult)
+                    it.resume(Unit)
+                }
+
+                override fun onError(
+                    composition: Composition,
+                    exportResult: ExportResult,
+                    exportException: ExportException
+                ) {
+                    super.onError(composition, exportResult, exportException)
+                    it.resumeWithException(exportException)
+                }
+            })
+            transformer.start(composition, tempFile.path)
+        }
+
+        return tempFile.toUri()
     }
 
     private fun getFileFromAssets(fileName: String): File {
